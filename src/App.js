038ca35162d2eec2fb757776f4aeeb2402bf969e
fixingwoo <fixingwoo@gmail.com>
2022-03-20 01:08:04 +0900

    refactor: styled-components props과 상속

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import styled from 'styled-components';
+
+const Parent = styled.div`
+  display: flex;
+`;
+
+const FirstBox = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const SecondBox = styled.div`
+  background-color: blue;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+
 function App() {
-  return null;
+  return (
+    <Parent>
+      <FirstBox>
+        <Text>Hello</Text>
+      </FirstBox>
+
+      <SecondBox />
+    </Parent>
+  );
 }
 
 export default App;
